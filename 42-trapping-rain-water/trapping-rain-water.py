d10758c4373a4b69acc6eb9765b5cec8dfe68aa1
Praroop Chanda <64160432+PraroopChanda@users.noreply.github.com>
2025-01-19 17:51:54 -0600

    Time: 9 ms (78.57%) | Memory: 19.2 MB (43.62%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,24 +1,19 @@
 class Solution:
-    def trap(self, height: List[int]) -> int:
-        max_left=[0 for x in range(len(height))]
-        max_right=[0 for x in range(len(height))]
-        min_lr=[0 for x in range(len(height))]
-        length=len(height)-1
-        area=0
-        for x in range(len(height)):
-            if x==0:
-                continue
-            if height[x-1]>max_left[x-1]: ## left to right pass
-                max_left[x]=height[x-1]
+    def trap(self, height: list[int]) -> int:
+        if not height:
+            return 0
+        start_pt,last_pt=0, len(height)-1
+        leftmax, rightmax=height[start_pt], height[last_pt]
+        res=0
+        while start_pt<last_pt:
+            ## need min(leftmax,rightmax) boundary for computing area
+            if leftmax<rightmax: ## right bar is higher so no need to look for it computing area
+                start_pt+=1 
+                leftmax=max(leftmax,height[start_pt])
+                res+=leftmax-height[start_pt] ## no need to check for negatives as leftmax is alread maximum, so min res i can get is 0
             else:
-                max_left[x]=max_left[x-1]
-            if height[length-x+1]>max_right[length-x+1]: ## right to left pass
-                max_right[length-x]=height[length-x+1]
-            else:
-                max_right[length-x]=max_right[length-x+1]
-        for y in range(len(height)):
-            min_lr[y]=min(max_left[y],max_right[y])
-            if min_lr[y]-height[y]>0:
-                area+=min_lr[y]-height[y]
-        return(area)
-        
+                last_pt-=1
+                rightmax=max(rightmax, height[last_pt])
+                res+=rightmax-height[last_pt]
+            
+        return res      
