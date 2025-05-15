d65573bf212efc5f0785de7d38828fd954a09560
Praroop Chanda <64160432+PraroopChanda@users.noreply.github.com>
2025-05-14 22:06:01 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (88.29%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,6 +1,6 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        output_list=[]
+        #output_list=[]
         # for x in range(1,len(nums)-1):
         #     if nums[x]>nums[x-1] and nums[x]>nums[x+1]:
         #         output_list.append(x)
@@ -11,22 +11,37 @@ class Solution:
         #         output_list.append(len(nums)-1)
         # else:
         #     output_list.append(0)   
+        #### --------------##########
+        # def checkleft(i):
+        #     if nums[i]<nums[max(0,i-1)]:
+        #         return False
+        #     return True
+        # def checkRight(i):
+        #     if nums[i]<nums[min(len(nums)-1,i+1)]:
+        #         return False
+        #     return True
 
-        def checkleft(i):
-            if nums[i]<nums[max(0,i-1)]:
-                return False
-            return True
-        def checkRight(i):
-            if nums[i]<nums[min(len(nums)-1,i+1)]:
-                return False
-            return True
+        # for x in range(len(nums)):
+        #     if checkleft(x) and checkRight(x):
+        #         output_list.append(x)
+        #         break
+        # return output_list[0] 
 
-        for x in range(len(nums)):
-            if checkleft(x) and checkRight(x):
-                output_list.append(x)
-                break
 
-        return output_list[0]       
+        ## trying to do using binary search log(n) solution
+        l=0
+        r=len(nums)-1
+        while l<=r:
+            mid =(l+r)//2  ##mid is the element that is being now used for comparison
+            if mid>0 and nums[mid]<nums[mid-1]:
+                ## change right 
+                r=mid-1
+            elif mid < len(nums)-1 and nums[mid]<nums[mid+1]:
+                l=mid+1
+            else:
+                return mid
+
+
 
                 
             
