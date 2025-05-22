e8729b9d711e6cc3a494ae20f46624e15f4a2968
Praroop Chanda <64160432+PraroopChanda@users.noreply.github.com>
2025-05-22 15:35:11 -0500

    Time: 4 ms (1.06%) | Memory: 17.7 MB (90.43%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -6,24 +6,34 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if p is None and q is None:
-            return True
-        if bool(p)!=bool(q):
-            return False    
-        queuep=deque([p])
-        queueq=deque([q])
-        while queuep and queueq:
-            currp=queuep.popleft()
-            currq=queueq.popleft()
-            if (currp.val!=currq.val) or (bool(currp.left)!=bool(currq.left)) or (bool(currp.right)!=bool(currq.right)):               
+        def check_same_tree(p,q):
+            if not p and not q:
+                return True
+            if not p or not q or p.val!=q.val: ## if both are null that has already passed and would had returned true
                 return False
-            if currp.left:
-                queuep.append(currp.left)
-            if currq.left:
-                queueq.append(currq.left)
-            if currp.right:
-                queuep.append(currp.right)
-            if currq.right:
-                queueq.append(currq.right) 
-        return True            
+            return check_same_tree(p.left,q.left) and check_same_tree(p.right,q.right) 
+        return check_same_tree(p,q)
+
+
+        ## done using BFS
+        # if p is None and q is None:
+        #     return True
+        # if bool(p)!=bool(q):
+        #     return False    
+        # queuep=deque([p])
+        # queueq=deque([q])
+        # while queuep and queueq:
+        #     currp=queuep.popleft()
+        #     currq=queueq.popleft()
+        #     if (currp.val!=currq.val) or (bool(currp.left)!=bool(currq.left)) or (bool(currp.right)!=bool(currq.right)):               
+        #         return False
+        #     if currp.left:
+        #         queuep.append(currp.left)
+        #     if currq.left:
+        #         queueq.append(currq.left)
+        #     if currp.right:
+        #         queuep.append(currp.right)
+        #     if currq.right:
+        #         queueq.append(currq.right) 
+        # return True            
         
