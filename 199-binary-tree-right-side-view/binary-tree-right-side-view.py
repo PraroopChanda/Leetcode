54465e2bde9a18139b26d5e454c12d1bb405d966
Praroop Chanda <64160432+PraroopChanda@users.noreply.github.com>
2025-05-22 22:28:17 -0500

    Time: 3 ms (8.56%) | Memory: 17.6 MB (98.24%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -10,21 +10,12 @@ class Solution:
             return []
         output=[]
         qu=deque([root])
-        y=0
         while qu:
-            # print(len(qu))
-            # output.append(qu[len(qu)-1])
             y=len(qu)-1
             for x in range(len(qu)):
-                print(x,len(qu)-1)
-                # if x==len(qu)-1:
-                #     y=1
-                    #print(len(qu)-1)
                 curr=qu.popleft()
-                #print(curr.val)
                 if x==y:
                     output.append(curr.val)
-                    #y=0
                 if curr.left:
                     qu.append(curr.left)
                 if curr.right:
