cbc813f6b2ef58d01452eb63fa7d95e4c058407c
Praroop Chanda <64160432+PraroopChanda@users.noreply.github.com>
2025-11-03 17:05:06 -0600

    Time: 3 ms (91.64%) | Memory: 17.6 MB (83.85%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -1,49 +1,35 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        if len(s)==1:
-            return [1]
-        ## logic is we first do one pass through the entire array and record which 
-        ## is the last occuring position of that element 
-        output=[]
-        dict_1={}
-        for x in range(len(s)):
-            dict_1[s[x]]=x
-        ## logic --> one partion's element should not be in next subset
-        ## so we keep an end pointer too keep track of max(last element) in the sequence
-        ## once that is reached --> split
-        seq=""
-        end=dict_1[s[0]]
-        for elem in range(len(s)):
-            if len(seq)==0:
-                seq+=s[elem]
-                end=dict_1[s[elem]]
-                if elem==len(s)-1:
-                    output.append(len(seq))
-                    return output
-            if dict_1[s[elem]]>=end and elem!=end:
-                seq+=s[elem]
-                end=dict_1[s[elem]]
-            elif dict_1[s[elem]]<end:
-                seq+=s[elem]
-            else:
-                seq+=s[elem]
-                output.append(len(seq)-1)
-                seq=""
-        return output    
-
-        ## could have done the same using seq=0 as well and then doing seq+1 at each time    
+            if len(s)==1:
+                return s
+            dict_1={}
+            for x in range(len(s)):
+                dict_1[s[x]]=x # capturing the last index (where each character ends)
+            output=[]
+            elem=""
+            farthest=0
+            for idx in range(len(s)):
+                if len(elem)==0:
+                    elem+=s[idx]
+                    farthest=dict_1[s[idx]]
+                    if farthest == idx:
+                        output.append(len(elem))
+                        elem=""
+                elif farthest>=dict_1[s[idx]] and farthest!=idx:
+                    elem+=s[idx]
+                elif farthest<dict_1[s[idx]]:
+                    elem+=s[idx]
+                    farthest=dict_1[s[idx]]
+                else:
+                    elem+=s[idx]
+                    output.append(len(elem))
+                    elem=""
+            return output              
 
 
 
 
 
-                
-
-
-
-        
-
-            
 
 
 
