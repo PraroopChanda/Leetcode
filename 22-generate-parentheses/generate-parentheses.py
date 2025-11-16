aab55fece6406ade1e651190e8ffe1b64a979e84
Praroop Chanda <64160432+PraroopChanda@users.noreply.github.com>
2025-11-16 00:17:27 -0600

    Time: 0 ms (100.00%) | Memory: 17.9 MB (69.78%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,23 +1,25 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        ## doing using recursion 
-        ## logic is closedN<openN (closed paranthesis should always be less than open      paranthesis)
-        ## for generation of different combinations use backtracking -- important observation
-        res=[]
-        stack1=[]
-        def backtracking(openN,closeN):
-            if openN==closeN==n:
-                res.append("".join(stack1))
+        result=[]
+        sol=""
+        opened=n
+        closed=n
+
+        def paranthesis(sol,opened,closed):
+            if len(sol)==2*n:
+                result.append(sol)
                 return
-            if openN<n:
-                stack1.append("(")
-                backtracking(openN+1,closeN)
-                stack1.pop() ## this is necessary for generating combinations
-                ## like last in first out
-                ## basically after this -- next combinations starts
-            if closeN<openN:
-                stack1.append(")")
-                backtracking(openN,closeN+1)
-                stack1.pop()
-        backtracking(0,0)
-        return res   
+
+            if opened:
+                sol+="("
+                paranthesis(sol,opened-1,closed)
+                sol=sol[:-1]
+
+            if closed>opened:
+                sol+=")"
+                paranthesis(sol,opened,closed-1)
+                sol=sol[:-1]
+
+        paranthesis(sol,opened,closed)
+        return result                
+
