866b917abc0d1ddb0cbaeece69e99958b3a5d99b
Praroop Chanda <64160432+PraroopChanda@users.noreply.github.com>
2026-01-21 16:14:26 -0600

    Time: 31 ms (5.22%) | Memory: 21.3 MB (6.63%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        output=[]
+        premap={x:[] for x in range(numCourses)}
+
+        for r,c in prerequisites:
+            premap[r].append(c)    
+        visited=set()
+
+        def dfs(cr):
+            if cr in visited:
+                return False
+            if cr in output: ## meaning it can be done
+                return True 
+            visited.add(cr)      
+            for nei in premap[cr]:
+                if not dfs(nei):
+                    return False
+            visited.remove(cr)        
+            output.append(cr)
+            return True
+
+        for x in range(numCourses):
+            if not dfs(x):
+                return []
+        return output        
+                
+
+
+
+        
