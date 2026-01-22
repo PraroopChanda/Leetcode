ad2ee85b443cd86f6783a5177928f89a4531dafb
Praroop Chanda <64160432+PraroopChanda@users.noreply.github.com>
2026-01-21 22:18:51 -0600

    Time: 3 ms (54.03%) | Memory: 19.8 MB (8.22%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -6,15 +6,12 @@ class Solution:
 
         def find(x): ## basically find its parent
             if x==parent[x]:
-                print("after getting inside parent",x)
                 return parent[x]
             else:
-                print("this is the parent",parent[x])
                 return find(parent[x])
 
         def union(a,b): ## the logic of how to combine 2 nodes/edges
             pa,pb =find(a),find(b)
-            print(pa,pb)
             if pa==pb: ## meaning there already exists a connection between the two
                 return False
             ## now compare their rank to decide parent and child relationship
@@ -24,43 +21,9 @@ class Solution:
                 parent[pb]=pa
             else:
                 parent[pb]=pa
-                print("parent changed to",parent)
                 rank[pa]+=rank[pb]
             return True         
 
         for u,v in edges:
             if not union(u,v):
                 return [u,v]
-
-
-
-
-
-
-        # output=[]
-        # visited=set()
-        # premap={x:[] for x in range(1,len(edges)+1)}
-        # for r,c in edges:
-        #     premap[r].append(c)
-        # def dfs(node,parent):
-        #     if node in visited:
-        #         output.append([parent,node])
-        #         return
-
-        #     visited.add(node)
-        #     for nei in premap[node]:
-        #         dfs(nei,node)
-
-        #     return
-
-        # node=next(iter(premap))
-        # print(node)
-        # dfs(node,-1)
-
-        # print(output)
-        # if len(output)==1:
-        #     return output[0]
-        # else:
-        #     return output[-1]    
-            
-        
